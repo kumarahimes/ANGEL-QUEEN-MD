96c603e39146f7d8f887c7010c3d36f54f4aa5e6
kumarahimes <136141710+kumarahimes@users.noreply.github.com>
2023-07-16 15:23:47 +0530

    Update _help.js

--- commands/_help.js
+++ commands/_help.js
@@ -6,8 +6,8 @@
  Licensed under the  GPL-3.0 License;
  You may not use this file except in compliance with the License.
  It is supplied in the hope that it may be useful.
- * @project_name : QueenNethu-Md
- * @author : MR.Hansamala <https://github.com/mrhansamala>
+ * @project_name : ANGEL-QUEEN-MD
+ * @author : MR.HIMESH <>
  * @description : Nethu,A Multi-functional whatsapp bot.
  * @version 0.0.6
  **/
