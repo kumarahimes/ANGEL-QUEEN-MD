441c84c169ee9d862cd956ad1d8e7cd7f8bd0206
kumarahimes <136141710+kumarahimes@users.noreply.github.com>
2023-07-18 12:08:47 +0530

    Update downloader.js

--- commands/downloader.js
+++ commands/downloader.js
@@ -269,21 +269,7 @@ cmd({
             let anu = search.videos[0];
             const getRandom = (ext) => {
                 return `${Math.floor(Math.random() * 10000)}${ext}`;
-               image: {
-                    url: anu.thumbnail,
-                },
-                caption:           
-╭───────────────◆
-│⿻ ${tlang().title} 
-│  *Youtube Player* ✨
-│⿻ *Title:* ${anu.title}
-│⿻ *Duration:* ${anu.timestamp}
-│⿻ *Viewers:* ${anu.views}
-│⿻ *Uploaded:* ${anu.ago}
-│⿻ *Author:* ${anu.author.name}
-╰────────────────◆⦿ *Url* : ${anu.url}
-`,     
-            };
+               };
             let infoYt = await ytdl.getInfo(anu.url);
             if (infoYt.videoDetails.lengthSeconds >= videotime) return citel.reply(`❌ Video file too big!`);
             let titleYt = infoYt.videoDetails.title;
