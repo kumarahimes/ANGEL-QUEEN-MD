19f6a7e35523718e8f71586d371f11ecf6d644d9
kumarahimes <136141710+kumarahimes@users.noreply.github.com>
2023-08-08 09:03:17 +0530

    Update esana.js

--- commands/esana.js
+++ commands/esana.js
@@ -32,24 +32,6 @@ cmd({
 })
 //====================================================================
 cmd({
-    pattern: "hirunews",
-    react: "🗞️",
-    desc: "hirutv news",
-    category: "news",
-    use : ".hirunews",
-    filename: __filename
-},
-async(MrHansamala, mek, m,{from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
-try{
-
-const latest_hiru_data = await hiru.latestHiru();
-await Secktor.sendMessage(from,  { image: { url: latest_hiru_data.img }, caption: `*${latest_hiru_data.title}*\n\n${latest_hiru_data.desc}\n\n${latest_hiru_data.link}\n\n *©ꜱʜᴇɴᴜ Qᴜᴇᴇɴ ᴍᴅ*`}, { quoted: mek })
-}
-catch(e){
-console.log(e)
-}})
-
-cmd({
     pattern: "derananews",
     react: "🗞️",
     desc: "Ada derana News news",
