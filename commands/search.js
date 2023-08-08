92b34390b79ca4ee036a89fb54d2f4f145385f04
kumarahimes <136141710+kumarahimes@users.noreply.github.com>
2023-08-08 11:59:02 +0530

    Update search.js

--- commands/search.js
+++ commands/search.js
@@ -60,6 +60,7 @@ cmd({
     //---------------------------------------------------------------------------
 cmd({
             pattern: "weather",
+            react: "🌡️",
             category: "search",
             desc: "Sends weather info about asked place.",
             use: '<location>',
@@ -133,6 +134,7 @@ cmd({
     //---------------------------------------------------------------------------
     cmd({
         pattern: "google",
+        react: "🌐",
         alias :['search','gsearch'],
         category: "search",
         desc: "Sends info of given query from Google Search.",
@@ -157,6 +159,7 @@ cmd({
     //---------------------------------------------------------------------------
 cmd({
             pattern: "image",
+            react: "🏞",
             alias :['img','imge'],
             category: "search",
             desc: "Searches Image on Google",
@@ -167,7 +170,7 @@ cmd({
             if (!text) return citel.reply("Provide me a query!")
             if (!text) return reply("Hey bie please tell me for which pic you're looking");
             let name1 = text.split("|")[0]
-            let name2 = text.split("|")[10] || `10`
+            let name2 = text.split("|")[5] || `5`
             citel.reply(`Sending ${name2} image(s) of ${name1} in chat`)
             let nn = name2
             for (let i = 0; i < nn; i++) {
@@ -178,7 +181,7 @@ cmd({
                         image: {
                             url: images,
                         },
-                        caption: `_Sector Image Search_\n*${name1}*`,
+                        caption: `_Angel Queen Image Search_\n*${name1}*`,
                         headerType: 4,
                     };
                     Void.sendMessage(citel.chat, buttonMessage, {
