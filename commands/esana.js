056f5590d1912f3576dd6f2fb6e4aea6b34a8373
kumarahimes <136141710+kumarahimes@users.noreply.github.com>
2023-08-10 09:11:05 +0530

    Update esana.js

--- commands/esana.js
+++ commands/esana.js
@@ -6,7 +6,7 @@ cmd({
     pattern: 'esana',
     alias: ['esananews','news'],
     desc: 'whatsapp beta infomation.',
-    category: 'downloader',
+    category: '🗞️NEWS',
     react: "📃",
     use: '<wbi>',
   },
